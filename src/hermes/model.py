941099367952d3be247788756ff8535545b1ab85
Chuck Hardin <chardin@gmail.com>
2026-02-23 21:01:51 -0700

    Added more objects

--- src/hermes/model.py
+++ src/hermes/model.py
@@ -47,14 +47,128 @@ class User(Base):
     username = Column(String, unique=True, nullable=False)
     full_name = Column(String, nullable=False)
     hashed_password = Column(String)
-#    routines = relationship('Routine', back_populates='user')
+    routines = relationship('Routine', back_populates='user')
+
+
+"""A relationship between an exercise and a routine in the Hermes system.
+
+This table holds and manages the details of the link between an
+exercise and a routine.
+
+Attributes:
+    exercise_id (int): The ID for the exercise.
+    routine_id (int): The ID for the routine.
+    order (int): The order in which this exercise
+        occurs within the routine.
+"""
+exercise_to_routine_table = Table(
+    'exercise_to_routine',
+    Base.metadata,
+    Column('exercise_id', ForeignKey('exercise.exercise_id')),
+    Column('routine_id', ForeignKey('routine.routine_id'), primary_key=True),
+    Column('order', Integer, primary_key=True),
+)
+
+
+class Routine(Base):
+    """A routine in the Hermes system.
+
+    This class holds and manages the details of a routine.
+
+    Attributes:
+        routine_id (int): The globally unique ID of the routine
+        user_id (int): The user ID of the user who owns the routine.
+        name (str): The name for the routine which is unique to that user.
+    """
+
+    __tablename__ = 'routine'
+    routine_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('user.user_id'), nullable=False)
+    name = Column(String, nullable=False)
+    user = relationship('User', back_populates='routines')
+    UniqueConstraint('user_id', 'name', name='uq_user_id_name',)
+
+    exercises = relationship('Exercise', secondary=exercise_to_routine_table)
+
+    def to_dict(self):
+        """Return a static dict of the data for the routine.
+
+        Returns a dict of the static daqta for the current routine,
+        suitable for rendering as JSON.
+        """
+        routine = {'name': self.name(),
+                   'user': {'full_name': self.user.full_name(),
+                            'username': self.user.username()},
+                   'exercises': map(
+                       lambda exercise: exercise.to_dict(), self.exercises())
+                   }
+        return routine
+
+
+class Exercise(Base):
+    """An exercise in the Hermes system.
+
+    This class holds and manages the details of an exercise.
+
+    Attributes:
+        exercise_id (int): The globally unique ID for the exercise.
+        name (str): The name of the exercise.
+        num_sets (int): The number of sets for the exercise.
+        num_reps (int): The number of reps per set for the exercise.
+        supplemental_desc (str): The supplemental description
+            for the exercise.  Optional.
+        reference_video_url (str): A link to the reference video
+            for the exercise.  Optional.
+        user_id (int): The user ID of the user who owns the routine.
+            A value of 0 means the exercise is generic and has no owner.
+    """
+
+    __tablename__ = 'exercise'
+    exercise_id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    num_sets = Column(Integer, nullable=False)
+    num_reps = Column(Integer, nullable=False)
+    supplemental_desc = Column(String)
+    reference_video_url = Column(String)
+    user_id = Column(Integer, ForeignKey('user.user_id'))
+    UniqueConstraint('user_id', 'name', name='uq_user_id_name_version',)
+    # properties = relationship(back_populates='exercise')
+    # moves: Mapped[List['Move']] = relationship(
+    #    back_populates='exercise',
+    #    order_by='order')
+
+    def to_dict(self):
+        """Return a static dict of the data for the exercise.
+
+        Returns a dict of the static daqta for the current exercise,
+        suitable for rendering as JSON.
+        """
+        exercise = {'name': self.name(),
+                    'num_sets': self.num_sets(),
+                    'num_reps': self.num_reps(),
+                    'supplemental_desc': self.supplemental_desc(),
+                    'reference_video_url': self.reference_video_url(),
+                    # 'properties': map(
+                    #    lambda property: prop.to_dict(), self.properties()),
+                    # 'moves': map(
+                    #    lambda move: move.to_dict(), self.moves()),
+                    }
+        return exercise
 
 
 def create_test_database():
+    """Populate a test database.
+
+    Populates a database suitable for performing unit tests.
+    """
     Base.metadata.create_all(engine)
 
     u0 = User(username='admin', full_name='Admin')
-    u1 = User(username='chardin', full_name='Chuck Hardin', hashed_password='dummy')
+    u1 = User(username='chardin', full_name='Chuck Hardin',
+              hashed_password='dummy')
     session.add(u0)
     session.add(u1)
+    session.commit()
+    r0 = Routine(user_id=u1.user_id, name='Sample Routine')
+    session.add(r0)
     session.commit()
