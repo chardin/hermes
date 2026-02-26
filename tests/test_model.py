8d6c751230b352d724a8d3b695b122373b503f82
Chuck Hardin <chardin@gmail.com>
2026-02-25 21:56:50 -0700

    Style corrections

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,52 +4,57 @@ import os
 import tempfile
 import uuid
 
+
 def set_up_sqlite_database():
-    temp_config_file=tempfile.NamedTemporaryFile(mode='w+t',delete=False)
-    engine="db:\n  engine: \'sqlite:///:memory:\'"
+    temp_config_file = tempfile.NamedTemporaryFile(mode='w+t', delete=False)
+    engine = "db:\n  engine: \'sqlite:///:memory:\'"
     temp_config_file.write(engine)
     temp_config_file.flush()
 
-    os.environ['HERMES_CONFIG_FILE']=temp_config_file.name
+    os.environ['HERMES_CONFIG_FILE'] = temp_config_file.name
     return temp_config_file
+
 
 def create_test_db():
     create_database()
 
-    u0 = User(user_id=str(uuid.uuid4()), username='admin', full_name='Admin')
-    u1 = User(user_id=str(uuid.uuid4()), username='chardin', full_name='Chuck Hardin',
-              hashed_password='dummy')
+    u0 = User(user_id=str(uuid.uuid4()), username='admin',
+              full_name='Admin')
+    u1 = User(user_id=str(uuid.uuid4()), username='chardin',
+              full_name='Chuck Hardin', hashed_password='dummy')
     add_to_session_and_commit([u0, u1])
 
-    r0 = Routine(routine_id=str(uuid.uuid4()), user_id=u1.user_id, name='Evening Routine')
-    e0 = Exercise(exercise_id=str(uuid.uuid4()), name='Supine Bridge', num_sets=3, num_reps=10,
-                  user_id=u1.user_id)
+    r0 = Routine(routine_id=str(uuid.uuid4()), user_id=u1.user_id,
+                 name='Evening Routine')
+    e0 = Exercise(exercise_id=str(uuid.uuid4()), name='Supine Bridge',
+                  num_sets=3, num_reps=10, user_id=u1.user_id)
     add_to_session_and_commit([r0, e0])
 
     r0.add_exercise(e0)
     add_to_session_and_commit([])
 
-sys.path.append(os.getenv("HERMES_SRC_DIR", os.getcwd()))
 
-temp_config_file=set_up_sqlite_database()
+sys.path.append(os.getenv("HERMES_SRC_DIR", os.getcwd()))
+temp_config_file = set_up_sqlite_database()
 
 from config import Config
-
-from model import User, Routine, Exercise, create_database,\
+from model import User, Routine, Exercise, create_database, \
     session, add_to_session_and_commit
 
 create_test_db()
 
 
 class TestModel(unittest.TestCase):
-    
+
     def test_user(self):
         user = session.query(User).filter(User.username == 'chardin').one()
         self.assertEqual(user.full_name, 'Chuck Hardin')
 
     def test_routine(self):
-        routine = session.query(Routine).filter(Routine.name == 'Evening Routine').one()
+        routine = session.query(Routine).\
+            filter(Routine.name == 'Evening Routine').one()
         self.assertEqual(len(routine.exercises), 1)
+
 
 os.unlink(temp_config_file.name)
 if __name__ == '__main__':
