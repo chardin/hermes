0077a3485a028538f73938cf4974045f38176dbe
Chuck Hardin <chardin@gmail.com>
2026-03-03 19:33:31 -0700

    First versionh that actually produces some sort of audio

--- src/hermes/app.py
+++ src/hermes/app.py
@@ -14,7 +14,7 @@ from sqlalchemy import exc
 from model import session, add_to_session_and_commit, RenderedPhrase
 import tempfile
 from gtts import gTTS
-from pydub import AudioSegment
+import pydub
 
 
 class AudioController:
@@ -24,11 +24,17 @@ class AudioController:
     audio files in the Hermes system.
 
     """
-    def __init__(self, engine='gtts', lang='en'):
+    def __init__(self, engine='gtts', lang='en', begin_set='Begin', begin_exercise='Begin',
+                 prompt_before_next_exercise='Pause',
+                 end_of_routine='Exercise routine finished.  Good job!'):
         self.lang = lang
         self.engine = engine
+        self.begin_set = begin_set
+        self.begin_exercise = begin_exercise
+        self.prompt_before_next_exercise = prompt_before_next_exercise
+        self.end_of_routine = end_of_routine
 
-    def _rendered_phrase_audio(self, phrase, force_regen=False):
+    def _rendered_phrase_audio_path(self, phrase, force_regen=False):
         mp3 = tempfile.NamedTemporaryFile(mode='w+b', suffix='.mp3', delete=False)
 
         rp = None
@@ -45,11 +51,15 @@ class AudioController:
             rp = None
 
         if (force_regen or not rp):
-            tts = gTTS(text=phrase, lang=self.lang)
-            tts.save(mp3.name)
+            if (phrase == ' '):
+                audio = pydub.AudioSegment.silent(duration=100)
+                audio.export(mp3.name, format='mp3')
+            else:
+                tts = gTTS(text=phrase, lang=self.lang)
+                tts.save(mp3.name)
             mp3.flush()
             mp3_data = mp3.read()
-            audio = AudioSegment.from_mp3(mp3.name)
+            audio = pydub.AudioSegment.from_mp3(mp3.name)
             duration = audio.duration_seconds
 
         if rp:
@@ -60,4 +70,59 @@ class AudioController:
         else:
             rp = RenderedPhrase(phrase=phrase, lang=self.lang, mp3_data=mp3_data, duration=duration)
             add_to_session_and_commit([rp])
+        mp3.close()
+        return mp3.name
+
+    def _padded_phrase(self, rp_path, padded_duration=0):        
+        audio = pydub.AudioSegment.from_file(rp_path, format='mp3')
+        padding_msec = (padded_duration - audio.duration_seconds) * 1000
+        if (padding_msec > 0):
+            audio = audio + pydub.AudioSegment.silent(duration=padding_msec)
+
+        padded_mp3 = tempfile.NamedTemporaryFile(mode='rb', suffix='.mp3', delete=False)
+        audio.export(padded_mp3.name, format='mp3')
+        padded_mp3.close()
+        return padded_mp3.name
+
+    def _build_sound_element_dict(self, routine):
+        sound_element_dict = {routine.routine_id: self._padded_phrase(self._rendered_phrase_audio_path(routine.name)),
+                              'begin_set': self._padded_phrase(self._rendered_phrase_audio_path(self.begin_set)),
+                              'begin_exercise': self._padded_phrase(self._rendered_phrase_audio_path(self.begin_exercise)),
+                              'prompt_before_next_exercise': self._padded_phrase(self._rendered_phrase_audio_path(self.prompt_before_next_exercise)),
+                              'end_of_routine': self._padded_phrase(self._rendered_phrase_audio_path(self.end_of_routine)),
+                              }
+        for exercise in routine.exercises:
+            if not (exercise.exercise_id in sound_element_dict):
+                sound_element_dict[exercise.exercise_id] = self._padded_phrase(self._rendered_phrase_audio_path(exercise.name))
+            for move in exercise.moves:
+                if not (move.move_id in sound_element_dict):
+                    sound_element_dict[move.move_id] = self._padded_phrase(self._rendered_phrase_audio_path(move.name), move.duration)
+
+        return sound_element_dict
+
+    def build_audio_for_routine(self, routine):
+        mp3 = tempfile.NamedTemporaryFile(mode='w+b', suffix='.mp3', delete=False)
+
+        sound_element_dict = self._build_sound_element_dict(routine)
+
+        audio = pydub.AudioSegment.from_file(sound_element_dict[routine.routine_id], format='mp3')
+
+        for exercise in routine.exercises:
+            audio = audio + pydub.AudioSegment.from_file(sound_element_dict[exercise.exercise_id], format='mp3')
+            
+            audio = audio + pydub.AudioSegment.from_file(sound_element_dict['begin_exercise'], format='mp3')
+
+            for i in range(1, exercise.num_sets):
+                for j in range(1, exercise.num_reps):
+                    for move in exercise.moves:
+                        audio = audio + pydub.AudioSegment.from_file(sound_element_dict[move.move_id], format='mp3')
+                        
+                audio = audio + pydub.AudioSegment.from_file(sound_element_dict['begin_set'], format='mp3')
+
+            audio = audio + pydub.AudioSegment.from_file(sound_element_dict['prompt_before_next_exercise'], format='mp3')
+
+        audio = audio + pydub.AudioSegment.from_file(sound_element_dict['end_of_routine'], format='mp3')
+
+        audio.export(mp3.name, format='mp3')
+
         return mp3.name
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,7 @@ import sys
 import os
 import tempfile
 import uuid
+import pydub
 
 
 def set_up_sqlite_database():
@@ -18,13 +19,48 @@ def set_up_sqlite_database():
 def create_test_db():
     create_database()
 
+    u0 = User(user_id=str(uuid.uuid4()), username='chardin',
+              full_name='Chuck Hardin', hashed_password='dummy')
+    
+    r0 = Routine(routine_id=str(uuid.uuid4()), user_id=u0.user_id,
+                 name='Evening Routine')
+    
+    e0 = Exercise(exercise_id=str(uuid.uuid4()), name='Cat-Camel',
+                  num_sets=2, num_reps=10, user_id=None)
+    e1 = Exercise(exercise_id=str(uuid.uuid4()), name='Supine Bridge',
+                  num_sets=3, num_reps=10, user_id=u0.user_id)
+    
+    r0.add_exercise(e0)
+    r0.add_exercise(e1)
+    
+    m00 = Move(move_id=str(uuid.uuid4()), exercise_id=e0.exercise_id,
+               order=0, duration=2, name='Neutral')
+    m01 = Move(move_id=str(uuid.uuid4()), exercise_id=e0.exercise_id,
+               order=1, duration=4, name='Arch')
+    m02 = Move(move_id=str(uuid.uuid4()), exercise_id=e0.exercise_id,
+               order=2, duration=5, name='Relax')
+    m03 = Move(move_id=str(uuid.uuid4()), exercise_id=e0.exercise_id,
+               order=3, duration=1, name=' ')
+    m11 = Move(move_id=str(uuid.uuid4()), exercise_id=e1.exercise_id,
+               order=1, duration=10, name='Hold')
+    m10 = Move(move_id=str(uuid.uuid4()), exercise_id=e1.exercise_id,
+               order=0, duration=3, name='Up')
+    m11 = Move(move_id=str(uuid.uuid4()), exercise_id=e1.exercise_id,
+               order=1, duration=10, name='Hold')
+    m12 = Move(move_id=str(uuid.uuid4()), exercise_id=e1.exercise_id,
+               order=2, duration=3, name='Down')
+    add_to_session_and_commit([u0, r0, e0, e1,
+                               m00, m01, m02, m03,
+                               m10, m11, m12])
+
 
 sys.path.append(os.getenv("HERMES_SRC_DIR", os.getcwd()))
 temp_config_file = set_up_sqlite_database()
 
 from app import AudioController
 from config import Config
-from model import RenderedPhrase, create_database, add_to_session_and_commit, session
+from model import RenderedPhrase, User, Routine, Exercise, Move, \
+    create_database, add_to_session_and_commit, session
 
 
 c = Config()
@@ -35,22 +71,32 @@ ac = AudioController(lang='en')
 
 class TestApp(unittest.TestCase):
 
-    def test__rendered_phrase_audio(self):
-        up_mp3_path = ac._rendered_phrase_audio('Up')
-        rp = session.query(RenderedPhrase).filter(RenderedPhrase.phrase == 'Up', RenderedPhrase.lang == 'en').one()
-        self.assertEqual(os.path.exists(up_mp3_path), True)
-        self.assertEqual(rp.duration, 0.624)
-        foo_mp3_path = ac._rendered_phrase_audio('Foo')
-        rp = session.query(RenderedPhrase).filter(RenderedPhrase.phrase == 'Foo', RenderedPhrase.lang == 'en').one()
-        self.assertEqual(rp.duration, 0.792)
-        self.assertEqual(os.path.exists(foo_mp3_path), True)
-        foo_mp3_path = ac._rendered_phrase_audio('Foo', force_regen=True)
-        self.assertEqual(os.path.exists(foo_mp3_path), True)
-        bar_mp3_path = ac._rendered_phrase_audio('Bar', force_regen=True)
-        self.assertEqual(os.path.exists(bar_mp3_path), True)
-        os.unlink(up_mp3_path)
-        os.unlink(foo_mp3_path)
-        os.unlink(bar_mp3_path)
+    def test__rendered_phrase_audio_path(self):
+        up_mp3_path = ac._rendered_phrase_audio_path('Up')
+        audio = pydub.AudioSegment.from_file(up_mp3_path)
+        self.assertTrue(abs(audio.duration_seconds - 0.624) < 0.01)
+        foo_mp3_path = ac._rendered_phrase_audio_path('Foo')
+        audio = pydub.AudioSegment.from_file(foo_mp3_path)
+        self.assertTrue(abs(audio.duration_seconds - 0.792) < 0.01)
+
+    def test__padded_phrase(self):
+        up_rp_path = ac._rendered_phrase_audio_path('Up')
+        padded_up_mp3_path = ac._padded_phrase(up_rp_path, 5)
+        mp3 = open(padded_up_mp3_path, 'br')
+        mp3_data = mp3.read()
+        audio = pydub.AudioSegment.from_mp3(mp3.name)
+        self.assertTrue((abs(audio.duration_seconds) - 5) < 0.01)
+        os.unlink(padded_up_mp3_path)
+
+    def test__build_sound_element_dict(self):
+        routine = session.query(Routine).filter(Routine.name == 'Evening Routine').one()
+        se_dict = ac._build_sound_element_dict(routine)
+        self.assertTrue('begin_set' in se_dict)
+
+    def test_build_audio_for_routine(self):
+        routine = session.query(Routine).filter(Routine.name == 'Evening Routine').one()
+        mp3_path = ac.build_audio_for_routine(routine)
+        print(mp3_path)
 
 temp_config_file.close()
 os.unlink(temp_config_file.name)
