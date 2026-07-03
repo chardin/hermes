bc92954809fc55af105a375fe7125a199d219b38
Chuck Hardin <chardin@gmail.com>
2026-07-03 15:33:20 -0600

    Proxy created in vite

--- src/hermes/app.py
+++ src/hermes/app.py
@@ -889,7 +889,7 @@ def profile():
     routines_to_serve = [r.to_dict(include_id=True) for r in user.routines]
     ac = AudioController()
     for r in routines_to_serve:
-        r['audio_path'] = 'http://192.168.12.226:5000/api/play_routine/' + r.get('routine_id', '')
+        r['audio_path'] = '/api/play_routine/' + r.get('routine_id', '')
     routines_to_serve.sort(key=lambda x: x.get('name', ''))
 
     return {'user': user.to_dict(), 'routines': routines_to_serve}
